3dd02cb76bfc2ed819a12f30475858cfa4bec855
Rezwanul Karim Prottay <71978110+prottay1968@users.noreply.github.com>
2024-12-18 00:38:21 -0500

    train.py

--- Seg_UKAN/train.py
+++ Seg_UKAN/train.py
@@ -154,13 +154,13 @@ def train(config, train_loader, model, criterion, optimizer):
             loss /= len(outputs)
 
             iou, dice, _ = iou_score(outputs[-1], target)
-            iou_, dice_, hd_, hd95_, recall_, specificity_, precision_,F1_ = indicators(outputs[-1], target)
+            iou_, dice_, hd_, hd95_, recall_, specificity_, precision_ = indicators(outputs[-1], target)
             
         else:
             output = model(input)
             loss = criterion(output, target)
             iou, dice, _ = iou_score(output, target)
-            iou_, dice_, hd_, hd95_, recall_, specificity_, precision_,F1_ = indicators(output, target)
+            iou_, dice_, hd_, hd95_, recall_, specificity_, precision_ = indicators(output, target)
 
         # compute gradient and do optimizing step
         optimizer.zero_grad()

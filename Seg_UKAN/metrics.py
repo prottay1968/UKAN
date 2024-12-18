71872c3313ffd5ebf53f314bd3a8c6adf2af6b37
Rezwanul Karim Prottay <71978110+prottay1968@users.noreply.github.com>
2024-12-18 12:05:25 -0500

    metrics.py

--- Seg_UKAN/metrics.py
+++ Seg_UKAN/metrics.py
@@ -56,7 +56,7 @@ def indicators(output, target):
     recall_ = recall(output_, target_)
     specificity_ = specificity(output_, target_)
     precision_ = precision(output_, target_)
-    f1_=0
+    f1_ = 2 * (precision_ * recall_) / (precision_ + recall_)
 
 
     return iou_, dice_, hd_, hd95_, recall_, specificity_, precision_,f1_
